bb46123e0f09209274e87a699587031cc3ca6f2f
varsh-inie <130482472+varsh-inie@users.noreply.github.com>
2023-08-09 10:04:01 +0530

    Add files via upload

---
+++ ex.1_c.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+ int a=0,n;
+ printf("enter n value");
+ scanf("%d",&n);
+ printf("Sri Vrashinie 192210706\n");
+ while(a<n){
+ 	a=a+2;
+ 	printf("%d\n",a);
+ }
+ return 0;
+ 	
+}
